3dbc46b6055c8e37cdddcc644d44b2bcb90ccb9d
linkadd515 <34799196+linkadd515@users.noreply.github.com>
2020-12-09 16:32:18 +0000

    Add files via upload

---
+++ StringManipulation/src/stringmanipulation/StringManipulation.java
@@ -0,0 +1,39 @@
+package stringmanipulation;
+
+
+public class StringManipulation {
+    
+
+
+    public static void main(String[] args) {
+        
+        String s1 = "Hello";
+        
+        lower(s1);
+        upper(s1);
+        equals(s1);
+        }
+public static void lower (String s1) {
+    String s2  = s1.toLowerCase();
+    System.out.println(s2);
+    }
+
+public static void upper(String s1){
+    String s2 = s1.toUpperCase();
+    System.out.println(s2);
+}
+
+public static void equals(String s1){
+    if (s1.equals("Hello")){
+        System.out.println("yes");
+    }else{
+        System.out.println("no");
+    }
+    
+}
+
+public static void 
+
+}
+    
+
